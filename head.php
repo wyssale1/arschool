907a5c2be74131cd8624e9008e65441dc027e1fc
wyssale1 <wyssale1@zhaw.ch>
2021-04-14 18:37:33 +0200

    Init

---
+++ head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+ <html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>ARSchool</title>
+        <meta name="description" content="AR School a app with helps to make interactive tasks">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="icon" type="image/svg+xml" href="img/favicon.svg">
+    </head>
---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+    include 'head.php';
+?>
+<body>
+        <header></header>
+        <main></main>
+        <footer></footer>
+        <script src="js/main.js" async defer></script>
+    </body>
+</html>
