dcc8dcd7fe7552c95b0eadb4303ec2594362fbc7
wyssale1 <wyssale1@students.zhaw.ch>
2021-08-26 16:31:08 +0200

    new

--- login.php
+++ login.php
@@ -31,6 +31,8 @@
                 <svg class="close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M48.31,9.85,33.16,25,48.31,40.15a5.77,5.77,0,1,1-8.16,8.16L25,33.16,9.85,48.31a5.77,5.77,0,1,1-8.16-8.16L16.84,25,1.69,9.85A5.77,5.77,0,0,1,9.85,1.69L25,16.84,40.15,1.69a5.77,5.77,0,1,1,8.16,8.16Z"/></svg>
                 <label for="name">Name</label>
                 <input type="text" name="name" id="name">
+                <label for="name">Passwort</label>
+                <input type="password" name="name" id="name">
                 <button id="create-room">Erstellen</button>
             </div>
         </template>
